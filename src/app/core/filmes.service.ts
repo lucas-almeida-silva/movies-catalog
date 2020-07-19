e3261b1c4c2a50fe4c969f8b9bfd0b65f02151a3
Lucas Almeida da Silva <lucas.silva@mttechne.com.br>
2020-07-19 11:14:57 -0300

    Exclusão de filmes

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -27,4 +27,8 @@ export class FilmesService {
     return this.http.get<Filme>(url + id);
   }
 
+  excluir(id: number): Observable<void> {
+    return this.http.delete<void>(url + id);
+  }
+
 }
--- src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
+++ src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
+import { Alerta } from 'src/app/shared/models/alerta';
+import { AlertaComponent } from 'src/app/shared/components/alerta/alerta.component';
 
 @Component({
   selector: 'dio-visualizar-filmes',
@@ -11,16 +14,43 @@ import { Filme } from 'src/app/shared/models/filme';
 export class VisualizarFilmesComponent implements OnInit {
   readonly semFoto = "https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg";
   filme: Filme;
+  id: number;
 
-  constructor(private activateRoute: ActivatedRoute,
+  constructor(public dialog: MatDialog,
+              private activateRoute: ActivatedRoute,
+              private router: Router,
               private filmesService: FilmesService) { }
 
   ngOnInit() {
-    this.visualizar(this.activateRoute.snapshot.params['id']);
+    this.id = this.activateRoute.snapshot.params['id'];
+    this.visualizar();
   }
 
-  private visualizar(id: number): void {
-    this.filmesService.visualizar(id).subscribe((filme: Filme) => {
+  editar(): void {
+    this.router.navigate(['filmes/cadastro', this.id]);
+  }
+
+  excluir(): void {
+    const config= { 
+      data: {
+        titulo: 'Você tem certeza que deseja excluir?',
+        descricao: 'Caso você tenha certeza que deseja excluir, clique no botão OK',
+        corBtnCancelar: 'primary',
+        corBtnSucesso: 'warn',
+        possuiBtnFechar: true
+      } as Alerta
+    }
+    const dialogRef = this.dialog.open(AlertaComponent, config);
+    dialogRef.afterClosed().subscribe(
+      (opcao:boolean) => {
+        if(opcao) {
+          this.filmesService.excluir(this.id).subscribe(() => this.router.navigate(['filmes']));
+        }
+      });
+  }
+
+  private visualizar(): void {
+    this.filmesService.visualizar(this.id).subscribe((filme: Filme) => {
       this.filme = filme;
     })
   }
