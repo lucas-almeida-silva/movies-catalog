ba18775aae876cadea0ef2ebcdaf3e1db897f7c3
Lucas Almeida da Silva <lucas.silva@mttechne.com.br>
2020-07-19 12:17:15 -0300

    Edição de filmes

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
@@ -14,29 +14,29 @@ import { Alerta } from 'src/app/shared/models/alerta';
   styleUrls: ['./cadastro-filmes.component.scss']
 })
 export class CadastroFilmesComponent implements OnInit {
-
+  id: number;
   registerForm: FormGroup;
   generos: Array<string>;
 
   constructor(public dialog: MatDialog,
               private fb: FormBuilder,
               private filmeService: FilmesService,
-              private router: Router) { }
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-
-    this.registerForm = this.fb.group({
-      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
-      urlFoto: ['', [Validators.minLength(10)]],
-      dtLancamento: ['', [Validators.required]],
-      descricao: [''],
-      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMDb: ['', Validators.minLength(10)],
-      genero: ['', [Validators.required]]
-    });
+    this.id = this.activatedRoute.snapshot.params['id'];
+    if(this.id) {
+      this.filmeService.visualizar(this.id).subscribe(
+        (filme: Filme) => {
+          this.criarFormulario(filme);
+        }
+      );
+    } else {
+      this.criarFormulario({} as Filme);
+    }
 
     this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Científica', 'Comédia', 'Drama'];
-
   } 
 
   submit(): void {
@@ -44,13 +44,24 @@ export class CadastroFilmesComponent implements OnInit {
     if(this.registerForm.invalid) {
       return;
     }
-
     const filme = this.registerForm.getRawValue() as Filme;
-    this.salvar(filme);
+    this.id ? this.editar(Object.assign({id: this.id}, filme)) : this.salvar(filme);
   }
 
   reiniciarForm(): void {
     this.registerForm.reset();
+  }
+
+  private criarFormulario(filme: Filme): void {
+    this.registerForm = this.fb.group({
+      titulo: [filme.titulo, [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: [filme.urlFoto, [Validators.minLength(10)]],
+      dtLancamento: [filme.dtLancamento, [Validators.required]],
+      descricao: [filme.descricao],
+      nota: [filme.nota, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: [filme.urlIMDb, Validators.minLength(10)],
+      genero: [filme.genero, [Validators.required]]
+    });
   }
 
   private salvar(filme: Filme): void {
@@ -68,7 +79,7 @@ export class CadastroFilmesComponent implements OnInit {
         dialogRef.afterClosed().subscribe(
           (opcao:boolean) => {
             if(opcao) {
-              this.router.navigateByUrl('filmes');
+              this.router.navigate(['filmes']);
             } else {
               this.reiniciarForm();
             }
@@ -80,6 +91,36 @@ export class CadastroFilmesComponent implements OnInit {
           data: {
             titulo: 'Erro ao salvar o registro!',
             descricao: 'Não conseguimos salvar seu registro, favor tentar novamente mais tarde',
+            btnSucesso: 'Fechar',
+            corBtnSucesso: 'warn'
+          } as Alerta
+        };
+        this.dialog.open(AlertaComponent, config);
+      }
+    )
+  }
+
+  private editar(filme: Filme): void {
+    this.filmeService.editar(filme).subscribe(
+      () => {
+        const config= { 
+          data: {
+            descricao: 'Seu registro foi atualizado com sucesso',
+            btnSucesso: 'Ir para a listagem'
+          } as Alerta
+        }
+        const dialogRef = this.dialog.open(AlertaComponent, config);
+        dialogRef.afterClosed().subscribe(
+          () => {
+            this.router.navigate(['filmes']);
+          }
+        )
+      },
+      () => {
+        const config= { 
+          data: {
+            titulo: 'Erro ao editar o registro!',
+            descricao: 'Não conseguimos editar seu registro, favor tentar novamente mais tarde',
             btnSucesso: 'Fechar',
             corBtnSucesso: 'warn'
           } as Alerta
