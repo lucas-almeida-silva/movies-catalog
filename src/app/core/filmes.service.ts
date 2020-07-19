a57df5adf8f45547d81b15a4e6f917f440d671aa
Lucas Almeida da Silva <lucas.silva@mttechne.com.br>
2020-07-19 10:42:15 -0300

    Componente de visualização dos dados dos filmes

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -5,7 +5,7 @@ import { Filme } from '../shared/models/filme';
 import { ConfigParams } from '../shared/models/config-params';
 import { ConfigParamsService } from './config-params.service';
 
-const url = 'http://localhost:3000/filmes';
+const url = 'http://localhost:3000/filmes/';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +21,10 @@ export class FilmesService {
   listar(config: ConfigParams): Observable<Filme[]> {
     const configParams = this.configService.configurarParametros(config);
     return this.http.get<Filme[]>(url, {params: configParams});
+  }
+
+  visualizar(id: number): Observable<Filme> {
+    return this.http.get<Filme>(url + id);
   }
 
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 import { debounceTime } from 'rxjs/operators'
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 import { ConfigParams } from 'src/app/shared/models/config-params';
-import { time } from 'console';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -21,10 +21,10 @@ export class ListagemFilmesComponent implements OnInit {
   filmes: Filme[] = [];
   filtrosListagem: FormGroup;
   generos: string[] 
-
   
   constructor(private filmesService: FilmesService,
-              private fb: FormBuilder) { }
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.filtrosListagem = this.fb.group({
@@ -51,6 +51,10 @@ export class ListagemFilmesComponent implements OnInit {
 
   onScroll(): void {
     this.listarFilmes();
+  }
+
+  abrir(id: number): void {
+    this.router.navigate(['filmes', id]);
   }
 
   listarFilmes(): void {
